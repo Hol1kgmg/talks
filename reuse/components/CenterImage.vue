fda4806d6911c0c6c4ef7f2e5f6dbf97c6b69d26
Hol1kgmg <abdevs.horisho@icloud.com>
2026-08-11 22:25:56 +0900

    fix:共通コンポーネントの不具合修正

--- reuse/components/CenterImage.vue
+++ reuse/components/CenterImage.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
-withDefaults(defineProps<{
+import { computed } from 'vue'
+
+const props = withDefaults(defineProps<{
   src: string
   imgClass?: string
 }>(), {
   imgClass: 'h-90 my-5',
 })
+
+const resolvedSrc = computed(() => `${import.meta.env.BASE_URL}${props.src.replace(/^\//, '')}`)
 </script>
 
 <template>
-  <img :src="src" class="mx-auto object-contain" :class="imgClass">
+  <img :src="resolvedSrc" class="mx-auto object-contain" :class="imgClass">
 </template>
